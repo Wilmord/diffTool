81560a28ef9bdc8c89ee4978ce20b38008077e6f
Berkay Elbir <berkayelbir10@hotmail.com>
2016-03-10 21:54:09 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This program compare two files and tells that they are identical or not. You can give filenames as arguments.
+
+Usage: diffTool.exe firstFile.txt secondFile.txt
---
+++ diffTool.cpp
@@ -0,0 +1,29 @@
+/*
+*@Author: Berkay Elbir
+* @Date  : 22.01.2016
+* @Info  : This program takes two files as arguments and compare them
+*		   by calling Windows System's diff command.
+*/
+
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+
+	std::string line1, line2;
+	if(argc != 3 )
+	{
+		std::cout << "usage: diffTool.exe <file_1> <file_2>" << std::endl;
+		return 0;
+	}
+
+	std::string systemCommand = "diff -q "; // It gives brief info. Files are different or not.
+	std::string firstFile(argv[1]);
+	std::string secondFile(argv[2]);
+	std::string files = firstFile.append(" ").append(secondFile);
+
+	std::string myCommand = systemCommand.append(files);
+	system(myCommand.c_str());
+
+	return 0;
+}
