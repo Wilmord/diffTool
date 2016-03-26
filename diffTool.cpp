1ac046d45d1df6279194aca5e9a796baebd4922e
Wilmord <berkayelbir10@hotmail.com>
2016-03-26 20:22:14 +0200

    Update diffTool.cpp

--- diffTool.cpp
+++ diffTool.cpp
@@ -1,5 +1,5 @@
 /*
-*@Author: Berkay Elbir
+*@Author: Wilmord
 * @Date  : 22.01.2016
 * @Info  : This program takes two files as arguments and compare them
 *		   by calling Windows System's diff command.
